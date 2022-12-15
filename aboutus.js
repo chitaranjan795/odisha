55c2b1a00ed7a2cdeb9ff7e7ed0f3550c1e71caf
chitaranjan795 <107648513+chitaranjan795@users.noreply.github.com>
2022-12-16 02:56:35 +0530

    this is final

--- aboutus.js
+++
@@ -1,7 +0,0 @@
-const hambutton = document.querySelector(".hamburger");
-const sidebar = document.querySelector(".list ul");
-
-hambutton.addEventListener('click', () => {
-    hambutton.classList.toggle('active');
-    sidebar.classList.toggle('active');
-})
